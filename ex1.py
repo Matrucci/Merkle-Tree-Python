665aadfd816af54aabc038fc51f0f1c26dd260e9
matrucci <matan.saloniko@gmail.com>
2021-06-10 18:50:26 +0300

    finished input 7

--- ex1.py
+++ ex1.py
@@ -9,6 +9,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 import base64
 
+#A class to represent a merkle tree node.
 class MerkleNode:
     def __init__(self, value):
         self.left = None
@@ -16,60 +17,37 @@ class MerkleNode:
         self.father = None
         self.value = hashlib.sha256(value.encode("UTF-8")).hexdigest()
 
-
+#Returns the tree root.
 def getRoot(node):
     while node.father != None:
         node = node.father
     return node
 
+#Adds a leaf to the tree.
 def addLeaf(leafs: List, plainText):
     leafs.append(MerkleNode(plainText))
 
-'''
+#Uses the leafs to calculate the tree root.
 def calcRoot(leafs: List):
-    #In case we already calculated the root of the tree.
-    if leafs[len(leafs) - 1].father != None:
-        root = leafs[len(leafs) - 1]
-        while root.father != None:
-            root = root.father
-        print(root.value)
-        return
-    #No leafs.
-    if len(leafs) == 0:
-        return
-    #There's just one leaf.
-    if len(leafs) == 1:
-        print(leafs[0].value)
-        return
-    #In case we need to calculate it.
-    for i in range(0, len(leafs), 2):
-        if (leafs[i].father == None or (i + 1 < len(leafs) and leafs[i + 1].father == None)): 
-            flag = False
-            joined = leafs[i].value
-            if i + 1 < len(leafs):
-                flag = True
-                joined = joined + leafs[i + 1].value
-            leafs[i].father = MerkleNode(joined)
-            leafs[i].father.left = leafs[i]
-            if flag:
-                leafs[i + 1].father = leafs[i].father
-                leafs[i + 1].father.right = leafs[i + 1]
-'''
-
-def calcRoot(leafs: List):
+    #If no leafs
     if len(leafs) == 0:
         return None
+    #If only 1 leaf
     if len(leafs) == 1:
         return leafs[0]
     parents = leafs
     temp = []
+    #Calculating each level of the tree.
     while len(parents) > 1:
+        #Going over the nodes in the level.
         for i in range(0, len(parents), 2):
             flag = False
             joined = parents[i].value
+            #Calculating the hash of both nodes.
             if i + 1 < len(parents):
                 flag = True
                 joined += parents[i + 1].value
+            #Creating a father.
             if flag == True:
                 node = MerkleNode(joined)
                 parents[i].father = node
@@ -85,25 +63,26 @@ def calcRoot(leafs: List):
                 temp.append(newNode)
         parents = temp
         temp = []
-        #print("THE LENGTH IS: " + str(len(parents)))
+    #Returning the root.
     return parents[0]
         
-
+#Creating proof of inclusion.
 def createPOI(leafs, strNum):
     leafNum = int(strNum)
     if leafNum >= len(leafs):
         return
-        #return error
+    #Calculating the tree root.
     root = calcRoot(leafs)
+    #Getting the desired leaf.
     node = leafs[leafNum]
     result = ""
+    #Checking if it has a brother from right or left.
     if leafNum % 2 == 1:
         result += "0" + node.father.left.value + " "
-        #result += node.value + " "
     else:
-        #result += node.value + " "
         if leafNum + 1 < len(leafs):
             result += "1" + node.father.right.value + " "
+    #Going over every level and adding the brother.
     while node.father.value != root.value:
         node = node.father
         if node == node.father.left:
@@ -115,13 +94,17 @@ def createPOI(leafs, strNum):
     result = root.value + " " + result
     result.strip()
     print(result)
-        
+
+#Checking and verifying proof of inclusion.    
 def checkPOI(leafValue, inputData):
+    #Calculating the hash of the input.
     hashedValue = hashlib.sha256(leafValue.encode("UTF-8")).hexdigest()
     if len(inputData) < 3:
-        print(False)
+        print("False")
         return
+    #Getting the tree root from the input POI.
     root = inputData[2]
+    #Going over the POI and calculating the hashes
     for i in range(3, len(inputData)):
         currentString = inputData[i]
         if currentString[0] == '0':
@@ -131,12 +114,14 @@ def checkPOI(leafValue, inputData):
             currentString = currentString[1:]
             currentHash = hashedValue + currentString
         hashedValue = hashlib.sha256(currentHash.encode("UTF-8")).hexdigest()
+    #Checking if the result hash is the same as the tree root.
     if root == hashedValue:
         print("True")
     else:
         print("False")
 
-        
+
+#Creating and printing RSA keys.
 def createRSAKeys():
     private_key = rsa.generate_private_key(
         public_exponent=65537,
@@ -155,13 +140,17 @@ def createRSAKeys():
         format=serialization.PublicFormat.SubjectPublicKeyInfo)
     print(pem.decode("UTF-8"))
 
+
+#Signing the current tree root with the input RSA key.
 def signRoot(key, root):
+    #Creating the private key from the input.
     privateKey = serialization.load_pem_private_key(
         key.encode(),
         password=None,
         backend=default_backend()
     )
 
+    #Signing the root.
     signature = privateKey.sign(
         root.value.encode(),
         padding.PSS(
@@ -169,19 +158,18 @@ def signRoot(key, root):
             salt_length=padding.PSS.MAX_LENGTH),
             hashes.SHA256())
     print(base64.b64encode(signature).decode("utf-8"))
-
-    #signature_encode = signature.decode("UTF-8")
-    #signature_base64 = base64.b64encode(signature_encode)
-    #print(signature_base64)
     
 
+#Checking the signature using the key provided and checking the decrypted message.
 def confirmSignature(key, signature, text):
+    #Creating the key from input.
     public_key = serialization.load_pem_public_key(
             key.encode(),
             backend=default_backend()
         )
     
     try:
+        #Verifying the signature.
         public_key.verify(
         base64.decodebytes(signature.encode()),
         text.encode(),
@@ -197,25 +185,41 @@ def confirmSignature(key, signature, text):
         print("False")
 
 
+def markLeaf(sparseLeafs, leafString):
+    leafNum = int(leafString, 16)
+    sparseLeafs.append(leafNum)
+
+#Wrong input.
+def wrongInput():
+    print("Wrong input. please select a number between 1-11")
+
 def main():
     leafs = []
+    sparseLeafs = []
     while True:
+        #Getting input.
         inputText = input()
         splitted = inputText.strip().split(' ')
+        #Input 1 - adding a leaf.
         if (splitted[0] == "1"):
             addLeaf(leafs, splitted[1])
+        #Input 2 - calculating tree root.
         elif (splitted[0] == "2"):
             root = calcRoot(leafs)
             if (root == None):
                 print()
             else:
                 print(root.value)
+        #Input 3 - creating proof of inclusion.
         elif (splitted[0] == "3"):
             createPOI(leafs, splitted[1])
+        #Input 4 - checking proof of inclusion.
         elif (splitted[0] == "4"):
             checkPOI(splitted[1], splitted)
+        #Input 5 - creating RSA keys.
         elif (splitted[0] == "5"):
             createRSAKeys()
+        #Input 6 - signing current tree root.
         elif (splitted[0] == "6"):
             longInput = inputText[2:] + "\n"
             shortInput = input()
@@ -223,6 +227,7 @@ def main():
                 longInput += shortInput + "\n"
                 shortInput = input()
             signRoot(longInput, calcRoot(leafs))
+        #Input 7 - verifying signature.
         elif (splitted[0] == "7"):
             longInput = inputText[2:] + "\n"
             shortInput = input()
@@ -232,14 +237,19 @@ def main():
             signatureInput = input()
             signatureArray = signatureInput.split(" ")
             confirmSignature(longInput, signatureArray[0], signatureArray[1])
+        #Input 8 - 
         elif (splitted[0] == "8"):
-            markLeaf()
+            markLeaf(sparseLeafs, splitted[1])
+        #Input 9 - 
         elif (splitted[0] == "9"):
             calcSparseRoot()
+        #Input 10 - 
         elif (splitted[0] == "10"):
             createSparsePOI()
+        #Input 11 - 
         elif (splitted[0] == "11"):
             checkSparsePOI()
+        #Wrong input
         else:
             wrongInput()
     return
